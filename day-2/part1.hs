1a3c0c1887537c45016b59f48d5e3005566cd861
Mark Hendriks <github@darkseraphim.net>
2019-12-02 18:21:39 +0100

    Add day 2

---
+++ day-2/part1.hs
@@ -0,0 +1,53 @@
+import Data.Map (Map, insert, (!), fromList)
+type IP = Integer
+type Memory = Map Integer Integer
+
+split :: Char -> String -> [String]
+split c (x:xs)
+    | x == c = [] : split c xs
+    | null xs = [[x]]
+    | otherwise = (x : head result) : tail result
+        where result = split c xs
+split c [] = []
+
+readInteger :: String -> Integer
+readInteger = read
+
+runAdd :: IP -> Memory -> Integer
+runAdd ip memory = run (ip + 4) newMemory
+  where addr1 = memory ! (ip + 1)
+        addr2 = memory ! (ip + 2)
+        addr3 = memory ! (ip + 3)
+        value1 = memory ! addr1
+        value2 = memory ! addr2
+        value3 = value1 + value2
+        newMemory = insert addr3 value3 memory
+
+runMult :: IP -> Memory -> Integer
+runMult ip memory = run (ip + 4) newMemory
+  where addr1 = memory ! (ip + 1)
+        addr2 = memory ! (ip + 2)
+        addr3 = memory ! (ip + 3)
+        value1 = memory ! addr1
+        value2 = memory ! addr2
+        value3 = value1 * value2
+        newMemory = insert addr3 value3 memory
+
+runExit :: IP -> Memory -> Integer
+runExit ip memory = memory ! 0
+
+run :: IP -> Memory -> Integer
+run ip memory
+  | op == 1 = runAdd ip memory
+  | op == 2 = runMult ip memory
+  | op == 99 = runExit ip memory
+    where
+          op = memory ! ip
+
+preprocess :: Memory -> Integer
+preprocess memory =
+  run 0 (insert 2 2 (insert 1 12 memory))
+
+main = do
+  commands <- fmap (map readInteger) (fmap (split ',') getLine)
+  print $ preprocess (fromList (zip [0..] commands))
---
+++ day-2/part2.hs
@@ -0,0 +1,59 @@
+import Data.Map (Map, insert, (!), fromList)
+type IP = Integer
+type Memory = Map Integer Integer
+
+split :: Char -> String -> [String]
+split c (x:xs)
+    | x == c = [] : split c xs
+    | null xs = [[x]]
+    | otherwise = (x : head result) : tail result
+        where result = split c xs
+split c [] = []
+
+readInteger :: String -> Integer
+readInteger = read
+
+runAdd :: IP -> Memory -> Integer
+runAdd ip memory = run (ip + 4) newMemory
+  where addr1 = memory ! (ip + 1)
+        addr2 = memory ! (ip + 2)
+        addr3 = memory ! (ip + 3)
+        value1 = memory ! addr1
+        value2 = memory ! addr2
+        value3 = value1 + value2
+        newMemory = insert addr3 value3 memory
+
+runMult :: IP -> Memory -> Integer
+runMult ip memory = run (ip + 4) newMemory
+  where addr1 = memory ! (ip + 1)
+        addr2 = memory ! (ip + 2)
+        addr3 = memory ! (ip + 3)
+        value1 = memory ! addr1
+        value2 = memory ! addr2
+        value3 = value1 * value2
+        newMemory = insert addr3 value3 memory
+
+runExit :: IP -> Memory -> Integer
+runExit ip memory = memory ! 0
+
+run :: IP -> Memory -> Integer
+run ip memory
+  | op == 1 = runAdd ip memory
+  | op == 2 = runMult ip memory
+  | op == 99 = runExit ip memory
+    where
+          op = memory ! ip
+
+preprocess :: Memory -> Integer -> Integer -> Integer
+preprocess memory noun verb =
+  -- Manually keep inserting numbers till it works
+  run 0 (insert 1 noun (insert 2 verb memory))
+
+answer :: [Integer] -> Integer
+answer (_:noun:verb:_) = noun * 100 + verb
+
+main = do
+  commands <- fmap (map readInteger) (fmap (split ',') getLine)
+  let memory = fromList (zip [0..] commands)
+  let outputs = [[(preprocess memory noun verb), noun, verb] | noun <- [0..99], verb <- [0..99]]
+  print $ answer $ head $ filter (\(output:noun:verb) -> output == 19690720) outputs
