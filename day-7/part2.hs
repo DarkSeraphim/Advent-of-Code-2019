13a3145e1c3936e54d0b0691a1442225df75cd20
Mark Hendriks <github@darkseraphim.net>
2019-12-09 00:27:19 +0100

    Complete part 2, with concurrency :)

--- day-7/part2.hs
+++ day-7/part2.hs
@@ -1,3 +1,5 @@
+import Control.Concurrent
+import Control.Concurrent.Chan
 import Data.List (permutations)
 import Data.Map (Map, insert, (!), fromList)
 import Helpers (split, readInteger)
@@ -6,7 +8,7 @@ type IP = Integer
 type Instruction = Integer
 type Mode = Integer
 type Memory = Map Integer Integer
-type Input = [Integer]
+type Input = [Chan (Maybe Integer)]
 type ParamMode = [Mode]
 
 readParameter :: IP -> Integer -> Memory -> ParamMode -> Integer
@@ -16,7 +18,7 @@ readParameter ip i memory modes
     where mode = modes !! (fromIntegral i)
           value = memory ! (ip + i)
 
-runAdd :: IP -> Memory -> Input -> ParamMode -> [Integer]
+runAdd :: IP -> Memory -> Input -> ParamMode -> IO ()
 runAdd ip memory input modes = run (ip + 4) newMemory input
   where value1 = readParameter ip 1 memory modes
         value2 = readParameter ip 2 memory modes
@@ -24,7 +26,7 @@ runAdd ip memory input modes = run (ip + 4) newMemory input
         value3 = value1 + value2
         newMemory = insert addr3 value3 memory
 
-runMult :: IP -> Memory -> Input -> ParamMode -> [Integer]
+runMult :: IP -> Memory -> Input -> ParamMode -> IO ()
 runMult ip memory input modes = run (ip + 4) newMemory input
   where value1 = readParameter ip 1 memory modes
         value2 = readParameter ip 2 memory modes
@@ -32,18 +34,22 @@ runMult ip memory input modes = run (ip + 4) newMemory input
         value3 = value1 * value2
         newMemory = insert addr3 value3 memory
 
-runRead :: IP -> Memory -> Input -> ParamMode -> [Integer]
-runRead ip memory input modes = run (ip + 2) newMemory newInput
-  where addr1 = memory ! (ip + 1)
-        (value:newInput) = input
-        newMemory = insert addr1 value memory -- Find a way to replace this with real input
+runRead :: IP -> Memory -> Input -> ParamMode -> IO ()
+runRead ip memory input modes = do
+                                  let addr1 = memory ! (ip + 1)
+                                  let cin = input !! 0
+                                  Just value <- readChan cin
+                                  let newMemory = insert addr1 value memory
+                                  run (ip + 2) newMemory input
 
-runPrint :: IP -> Memory -> Input -> ParamMode -> [Integer]
-runPrint ip memory input modes = value : (run (ip + 2) newMemory input)
-  where value = readParameter ip 1 memory modes
-        newMemory = memory
+runPrint :: IP -> Memory -> Input -> ParamMode -> IO ()
+runPrint ip memory input modes = do
+                                  let value = readParameter ip 1 memory modes
+                                  let cout = input !! 1
+                                  writeChan cout (Just value)
+                                  run (ip + 2) memory input 
 
-runJumpIfTrue :: IP -> Memory -> Input -> ParamMode -> [Integer]
+runJumpIfTrue :: IP -> Memory -> Input -> ParamMode -> IO ()
 runJumpIfTrue ip memory input modes
   | condition == True = run value2 memory input
   | otherwise = run (ip + 3) memory input
@@ -51,7 +57,7 @@ runJumpIfTrue ip memory input modes
         value2 = readParameter ip 2 memory modes
         condition = value1 /= 0
 
-runJumpIfFalse :: IP -> Memory -> Input -> ParamMode -> [Integer]
+runJumpIfFalse :: IP -> Memory -> Input -> ParamMode -> IO ()
 runJumpIfFalse ip memory input modes
   | condition == True = run value2 memory input
   | otherwise = run (ip + 3) memory input
@@ -59,7 +65,7 @@ runJumpIfFalse ip memory input modes
         value2 = readParameter ip 2 memory modes
         condition = value1 == 0
 
-runLessThan :: IP -> Memory -> Input -> ParamMode -> [Integer]
+runLessThan :: IP -> Memory -> Input -> ParamMode -> IO ()
 runLessThan ip memory input modes = run (ip + 4) newMemory input
   where value1 = readParameter ip 1 memory modes
         value2 = readParameter ip 2 memory modes
@@ -67,7 +73,7 @@ runLessThan ip memory input modes = run (ip + 4) newMemory input
         result = if value1 < value2 then 1 else 0
         newMemory = insert addr3 result memory
 
-runEquals :: IP -> Memory -> Input -> ParamMode -> [Integer]
+runEquals :: IP -> Memory -> Input -> ParamMode -> IO ()
 runEquals ip memory input modes = run (ip + 4) newMemory input
   where value1 = readParameter ip 1 memory modes
         value2 = readParameter ip 2 memory modes
@@ -75,8 +81,8 @@ runEquals ip memory input modes = run (ip + 4) newMemory input
         result = if value1 == value2 then 1 else 0
         newMemory = insert addr3 result memory
 
-runExit :: IP -> Memory -> Input -> ParamMode -> [Integer]
-runExit ip memory modes input = []
+runExit :: IP -> Memory -> Input -> ParamMode -> IO ()
+runExit ip memory input modes = writeChan (input !! 1) Nothing
 
 parseMode :: Integer -> Integer -> Mode
 parseMode i op = (op `div` (10 ^ (i + 1))) `mod` 10
@@ -90,7 +96,7 @@ parseModes op = (ins, [-1, param1, param2, param3])
           param2 = parseMode 2 op
           param3 = parseMode 3 op
 
-run :: IP -> Memory -> Input -> [Integer]
+run :: IP -> Memory -> Input -> IO ()
 run ip memory input
   | ins == 1 = runAdd ip memory input modes
   | ins == 2 = runMult ip memory input modes
@@ -104,15 +110,42 @@ run ip memory input
     where op = memory ! ip
           (ins, modes) = parseModes op
 
-preprocess :: Memory -> Input -> [Integer]
-preprocess memory input =
-  run 0 memory input
+preprocess :: Memory -> [Integer] -> IO Integer
+preprocess memory phases = do
+  chanA <- newChan
+  chanB <- newChan
+  chanC <- newChan
+  chanD <- newChan
+  chanE <- newChan
+  output <- dupChan chanA
+  forkIO (run 0 memory [chanA, chanB])
+  forkIO (run 0 memory [chanB, chanC])
+  forkIO (run 0 memory [chanC, chanD])
+  forkIO (run 0 memory [chanD, chanE])
+  forkIO (run 0 memory [chanE, chanA])
+  writeChan chanA (Just (phases !! 0))
+  writeChan chanB (Just (phases !! 1))
+  writeChan chanC (Just (phases !! 2))
+  writeChan chanD (Just (phases !! 3))
+  writeChan chanE (Just (phases !! 4))
+  writeChan chanA (Just (toInteger 0))
+  results <- waitForExit (Just (toInteger 0)) output
+  return (last results)
 
-runAmps :: Memory -> Input -> Integer
-runAmps memory inputs = foldl (\lastOutput newInput -> last (preprocess memory [newInput, lastOutput])) 0 inputs
+unpack :: Maybe a -> a -> a
+unpack (Just a) b = a
+unpack Nothing b = b
+
+waitForExit :: Maybe a -> Chan (Maybe a) -> IO [a]
+waitForExit (Just v) c = do
+            r <- readChan c
+            z <- waitForExit r c
+            return (v:z)
+waitForExit Nothing c = return []
 
 main = do
   commands <- fmap (map readInteger) (fmap (split ',') getLine)
   let memory = fromList (zip [0..] commands)
   let inputs = permutations [5..9]
-  print $ maximum (map (runAmps memory) inputs)
+  results <- sequence (map (preprocess memory) inputs)
+  print (maximum results)
